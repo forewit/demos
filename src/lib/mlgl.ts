0dd5ca30fc8e7adf9c4806ff78318f76597d3ea0
Anderson <MDAnderson@samaritan.org>
2023-07-18 16:05:31 -0400

    'na'

--- src/lib/mlgl.ts
+++ src/lib/mlgl.ts
@@ -21,6 +21,7 @@
  * 1. create shader modules (packaged wgsl code)
  * 2. setup the canvas context (optional for compute only pipelines)
  * 3. store data in buffers or textures on the gpu
+ *    buffer types: index, uniform, vertex, storage, map_write, copy_dst, indirect, etc... |
  * 4. create pipelines (describe data structure, bindings, shaders, and resource layouts)
  *      A. render pipeline (vertex and fragment stages)
  *      B. compute pipeline (singe compute stage)
@@ -29,26 +30,28 @@
  *      B. encoder object -- the commands run on that object
  *      C. command list -- specifies which pipeline, buffers, and how many draw operations
  *      D. merge it all into a command buffer
+ * 
+ * todo: learn types of gpu buffers 
  */
 
 
-//! Utility Functions ********************
+// Utility Functions ********************
 function sigmoid(x: number): number { return 1 / 1 + Math.exp(-x) }
 function logit(x: number): number { return Math.log(x / (1 - x)) }
 
-//! Exports ******************************
+// Exports ******************************
 export function ml_train() { } // returns a model
 export function ml_run() { } // returns output of a model given inputs
 export let ml_init = initWebGPU
 
-//! Setup webgpu *************************
+// Setup webgpu *************************
 // Define global buffer size
-const BUFFER_SIZE = 64; // bytes (must be multiple of 4)
+const BUFFER_SIZE = 1000; // bytes (must be multiple of 4)
 
 // Compute shader
 const shader = `
-@group(0) @binding(0)
-var<storage, read_write> output: array<f32>;
+@group(0) @binding(0) var<storage, read_write> output: array<f32>;
+@group(0) @binding(1) var<storage, read_write> output2: array<f32>;
 
 @compute @workgroup_size(64)
 fn main(
@@ -65,81 +68,81 @@ fn main(
 
   output[global_id.x] =
     f32(global_id.x) * 1000. + f32(local_id.x);
+
+  output2[global_id.x] = f32(global_id.x);
 }
 `;
 
 async function initWebGPU() {
-  //! 1: request adapter and device
+  // 1: request adapter and device
   if (!navigator.gpu) {
     throw Error('WebGPU not supported.');
   }
 
-  const adapter = await navigator.gpu.requestAdapter({powerPreference: "high-performance"}); // or "low-power"
+  const adapter = await navigator.gpu.requestAdapter({ powerPreference: "high-performance" }); // or "low-power"
   if (!adapter) throw Error('Couldn\'t request WebGPU adapter.');
   if (adapter.isFallbackAdapter) console.warn("WebGPU is using a fallback adapter!")
 
   const device = await adapter.requestDevice(); // you can pass options to request a device with specific GPU features
 
-  //! 2: Create a shader module from the shader template literal
+  // 2: Create a shader module from the shader template literal
   const shaderModule = device.createShaderModule({
     code: shader
   });
 
-  //! 3: Create an output buffer to read GPU calculations to, and a staging buffer to be mapped for JavaScript access
-
-  const output = device.createBuffer({
+  // 3: Create an output buffer to read GPU calculations to, and a staging buffer to be mapped for JavaScript access
+  const outputBuffer = device.createBuffer({
     size: BUFFER_SIZE,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
+  });
+
+  const outputBuffer2 = device.createBuffer({
+    size:  BUFFER_SIZE,
     usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
   });
 
   const stagingBuffer = device.createBuffer({
     size: BUFFER_SIZE,
-    usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
+    usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST // MAP_READ allows reading from gpu to JS
   });
 
-  //! 4: Create a GPUBindGroupLayout to define the bind group structure, create a GPUBindGroup from it,
-  // then use it to create a GPUComputePipeline
-
-  const bindGroupLayout =
-  device.createBindGroupLayout({
-    entries: [{
-      binding: 0,
-      visibility: GPUShaderStage.COMPUTE,
-      buffer: {
-        type: "storage"
-      }
-    }]
-  });
-
-  const bindGroup = device.createBindGroup({
-    layout: bindGroupLayout,
-    entries: [{
-      binding: 0,
-      resource: {
-        buffer: output,
-      }
-    }]
-  });
-
+  // 4: Create a compute pipeline using layout: "auto" to automatically generate
+  // appropriate bind group layouts
   const computePipeline = device.createComputePipeline({
-    layout: device.createPipelineLayout({
-      bindGroupLayouts: [bindGroupLayout]
-    }),
+    layout: "auto",
     compute: {
       module: shaderModule,
       entryPoint: 'main'
     }
   });
 
-  //! 5: Create GPUCommandEncoder to issue commands to the GPU
+  // Create a bind group with the auto-generated layout from the compute pipeline
+  const bindGroup = device.createBindGroup({
+    layout: computePipeline.getBindGroupLayout(0),
+    entries: [{
+      binding: 0,
+      resource: {
+        buffer: outputBuffer,
+      }
+    },{
+      binding: 1,
+      resource: {
+        buffer: outputBuffer2,
+      }
+    }]
+  });
+
+  // 5: Create GPUCommandEncoder to issue commands to the GPU
   const commandEncoder = device.createCommandEncoder();
 
-  //! 6: Initiate render pass
+  // 6: Initiate render pass
   const passEncoder = commandEncoder.beginComputePass();
-    
-  //! 7: Issue commands
+
+  // 7: Issue commands
   passEncoder.setPipeline(computePipeline);
   passEncoder.setBindGroup(0, bindGroup);
+  passEncoder.dispatchWorkgroups(Math.ceil(BUFFER_SIZE / 64));
+  passEncoder.setBindGroup(1, bindGroup);
   passEncoder.dispatchWorkgroups(Math.ceil(BUFFER_SIZE / 64));
 
   // End the render pass
@@ -147,14 +150,14 @@ async function initWebGPU() {
 
   // Copy output buffer to staging buffer
   commandEncoder.copyBufferToBuffer(
-    output,
+    outputBuffer2,
     0, // Source offset
     stagingBuffer,
     0, // Destination offset
     BUFFER_SIZE
   );
 
-  //! 8: End frame by passing array of command buffers to command queue for execution
+  // 8: End frame by passing array of command buffers to command queue for execution
   device.queue.submit([commandEncoder.finish()]);
 
   // map staging buffer to read results back to JS
