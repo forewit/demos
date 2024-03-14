ad98f5ddaa2d3d55ee634faf02427b3f4a33b7cc
Anderson <MDAnderson@samaritan.org>
2024-03-14 15:28:26 -0400

    'na'

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
+			base: process.env.NODE_ENV === "production" ? process.env.BASE_PATH : "",
 		}
 	}
 };
