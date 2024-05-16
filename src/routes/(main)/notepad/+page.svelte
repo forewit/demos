59292dcef8d0f060513e6f4a68d521aaf54310b1
Anderson <MDAnderson@samaritan.org>
2024-05-16 14:47:10 -0400

    'na'

--- src/routes/(main)/notepad/+page.svelte
+++ src/routes/(main)/notepad/+page.svelte
@@ -1,1 +1,75 @@
-<h1>notepad</h1>
+<div class="grid">
+  <div class="tabs"></div>
+  <div class="toolbar"></div>
+  <div class="editor">
+    <textarea name="editor" id="text"></textarea>
+  </div>
+  <div class="status-bar"></div>
+</div>
+
+<style>
+  .grid {
+    --text-color: #fff;
+    --background-color: #272727;
+    --toolbar-color: #1a2220;
+    --highlight-color: #9f9f9f; /* scrollbar, badges, etc.*/
+    --dark-color: #000e0f;
+    --font-family: "Consolas", monospace;
+
+    height: 100%;
+    display: grid;
+    grid-template-rows: 2rem 2rem 1fr 2rem;
+  }
+
+  .tabs {
+    background: var(--dark-color);
+  }
+  .toolbar {
+    background: var(--toolbar-color);
+  }
+  .editor {
+    display: grid;
+    grid-template-columns: 1fr 5px; /*spacing for scrollbar*/
+    background-color: var(--background-color);
+  }
+  #text {
+    resize: none;
+    font-family: var(--font-family);
+    color: var(--text-color);
+    background: var(--background-color);
+
+    padding: 1rem;
+    border: none;
+    outline: none;
+    width: calc(100%-1rem);
+    height: 100%;
+    width: 100%;
+  }
+  #text::-webkit-scrollbar {
+    width: 5px;
+    color: white;
+    /**add margin top and bottom*/
+    margin-inline: 0.5rem;
+  }
+  #text::-webkit-scrollbar-thumb {
+    background-color: var(--highlight-color);
+    border-radius: 8px;
+  }
+  #text::-webkit-scrollbar-button:vertical:decrement {
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-image: url('data:image/svg+xml,<svg width="16px" height="16px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill="%239f9f9f" d="M8 1.25a2.101 2.101 0 00-1.785.996l.64.392-.642-.388-5.675 9.373-.006.01a2.065 2.065 0 00.751 2.832c.314.183.67.281 1.034.285h11.366a2.101 2.101 0 001.791-1.045 2.064 2.064 0 00-.006-2.072L9.788 2.25l-.003-.004A2.084 2.084 0 008 1.25z"/></svg>');
+  }
+  #text::-webkit-scrollbar-button:vertical:increment {
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-image: url('data:image/svg+xml,<svg width="16px" height="16px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none" transform="rotate(180)"><path fill="%23f9f9f9" d="M8 1.25a2.101 2.101 0 00-1.785.996l.64.392-.642-.388-5.675 9.373-.006.01a2.065 2.065 0 00.751 2.832c.314.183.67.281 1.034.285h11.366a2.101 2.101 0 001.791-1.045 2.064 2.064 0 00-.006-2.072L9.788 2.25l-.003-.004A2.084 2.084 0 008 1.25z"/></svg>');
+  }
+
+  .status-bar {
+    /* theme */
+    background: var(--toolbar-color);
+  }
+</style>
